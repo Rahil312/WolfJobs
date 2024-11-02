1f2aecc6b9c6a25d7a23955a7ac315e2ec89fca4
RohanChampaneri11 <75676632+RohanChampaneri11@users.noreply.github.com>
2024-11-01 21:18:30 -0400

    Updated authOtp.js
    Added two factor authentication One time password sending functionality.

--- backend/models/authOtp.js
+++ backend/models/authOtp.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
-// Auth OTP table
 const autoOtpSchema = new mongoose.Schema({
-  userId: {
+  email: {
     type: String,
     required: true
   },
@@ -12,14 +12,36 @@ const autoOtpSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+    expires: 60 * 5, 
+  },
 
 }, {
   timestamps: true
 });
 
-
-const autoOtp = mongoose.model('autoOtp', autoOtpSchema);
-
-module.exports = autoOtp;
+async function sendVerificationEmail(email, otp) {
+  try {
+    console.log("Attempting to send email to:", email);
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email",
+      `<h1>Please confirm your OTP</h1>
+       <p>Here is your OTP code: ${otp}</p>`
+    );
+    console.log("Email sent successfully: ", mailResponse);
+  } catch (error) {
+    console.log("Error occurred while sending email: ", error);
+    throw error;
+  }
+}
+autoOtpSchema.pre("save", async function (next) {
+  console.log("Pre-save hook triggered for new OTP document");
+  console.log("New document saved to the database");
+  // Only send an email when a new document is created
+  if (this.isNew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
+  next();
+});
+module.exports = mongoose.model("OTP", autoOtpSchema);
